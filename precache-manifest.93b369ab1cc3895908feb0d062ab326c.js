466edb31b3697066d6ae8913b8c423e0f357b6b3
eduardo-nakamura <edu-nakamura@hotmail.com>
2020-08-29 18:18:28 -0300

    Updates

---
+++ precache-manifest.93b369ab1cc3895908feb0d062ab326c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c3c6d2baae7f98deaaf68865e983e99",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "8c1771d7e116769e9d3d",
+    "url": "/pokedex/static/css/main.92015283.chunk.css"
+  },
+  {
+    "revision": "1f6591bc77887489049a",
+    "url": "/pokedex/static/js/2.a0fecf18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.a0fecf18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1771d7e116769e9d3d",
+    "url": "/pokedex/static/js/main.89220249.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
