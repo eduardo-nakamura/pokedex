559662465ed26625c8af06a52087273e53833f4d
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2020-08-31 20:13:25 -0300

    Updates

--- precache-manifest.0f0cfeeda788852433b9f8b89f50ddaf.js
+++ precache-manifest.0f0cfeeda788852433b9f8b89f50ddaf.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "21fd1f1da1450b1ea3e39bc4f51f3bac",
+    "revision": "96584fcc2b97f5dc51d54f88267c7e22",
     "url": "/pokedex/index.html"
   },
   {
-    "revision": "92f9fb3b32ea201286db",
+    "revision": "84beba34d2e930943163",
     "url": "/pokedex/static/css/main.a9621143.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pokedex/static/js/2.5c0f06cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92f9fb3b32ea201286db",
-    "url": "/pokedex/static/js/main.63bf5fa6.chunk.js"
+    "revision": "84beba34d2e930943163",
+    "url": "/pokedex/static/js/main.173b952c.chunk.js"
   },
   {
     "revision": "5d5b7317cf43a0b8925c",
